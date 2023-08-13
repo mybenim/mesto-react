5e8ca23ad5ffcb986a2276251c9557a2edf68a51
Inna Golovko <agro-inna@mail.ru>
2023-08-13 12:16:12 +0500

    на проверку пр-10

--- src/components/ImagePopup/ImagePopup.jsx
+++ src/components/ImagePopup/ImagePopup.jsx
@@ -1,4 +1,4 @@
-export default function PopupImage({ card, isOpen, onClose }) {
+export default function ImagePopup({ card, isOpen, onClose }) {
     return (
     <section className={`popup popup-image ${isOpen && "popup_opened"}`}>
         <div className="popup__container popup__container-image">
